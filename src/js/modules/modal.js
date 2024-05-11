cc4d02d7a03523bd8683094c36cb865b59b4e6b2
gleb <glebanuk12387@gmail.com>
2024-05-11 12:52:56 +0300

    открытие модальных окон и всплытие модального окна спустя по таймеру

---
+++ src/js/modules/modal.js
@@ -0,0 +1,95 @@
+const modals = () => {
+
+  function bindModal(triggerSelector, modalSelector, closeSelector, closeCLickOverlay = true)  { // 'closeCLickOverlay' для контроля закрытия мод окна на подложку(какое будет а какое нет)
+    const trigger = document.querySelectorAll(triggerSelector), // можно повесить на несколько селекторов одни и те же функции 
+          modal = document.querySelector(modalSelector),
+          close = document.querySelector(closeSelector),
+          windows = document.querySelectorAll('[data-modal]'), // получаем все модальные окна чтоб они не наслаивались друг на друга как в modal_calc
+          scroll = calcScroll(); // чтоб страница не дургалась при исчезновении скрола
+        
+    trigger.forEach(item => {
+      item.addEventListener('click', (e) =>  {
+        if (e.target) {
+            e.preventDefault();
+        }
+
+        windows.forEach(item  =>  { // этот код закрывает все ненужные открытые окна
+          item.style.display = 'none';
+        });
+
+        modal.style.display = 'block';
+        document.body.style.overflow  = 'hidden';
+        document.body.style.marginRight = `${scroll}px`;//// чтоб страница не дургалась при исчезновении скрола
+
+        // document.body.classList.add('modal-open');
+
+      });
+    });
+
+    close.addEventListener('click', ()  =>  {
+      modal.style.display = 'none';
+      document.body.style.overflow  = '';
+      document.body.style.marginRight = `0px`;
+
+      // document.body.classList.remove('modal-open');
+    });
+
+
+    modal.addEventListener('click', (e)  =>  {
+      if (e.target === modal && closeCLickOverlay)  {
+
+        windows.forEach(item  =>  {
+          item.style.display = 'none';
+        });
+
+        modal.style.display = 'none';
+        document.body.style.overflow  = '';
+        document.body.style.marginRight = `0px`;
+
+        // document.body.classList.remove('modal-open');
+      }
+    })
+  }
+
+  function  showModalByTime(selector, time) { // если еще ни одно мод окно не открыто то показываем мод окно через какое-то время
+    setTimeout(() => {
+      let display;
+
+      document.querySelectorAll('[data-modal]').forEach(item =>  {
+        if  (getComputedStyle(item).display !== 'none') {
+          display = 'block'; 
+        }
+      });
+
+      if  (!display)  {
+        document.querySelector(selector).style.display = 'block';
+        document.body.style.overflow  = 'hidden';
+      }
+
+
+    }, time);
+  }
+
+  function calcScroll() { 
+    let div = document.createElement('div');
+
+    div.style.width = '50px';
+    div.style.height = '50px';
+    div.style.overflowY = 'scroll';
+    div.style.visibility = 'hidden';
+
+    document.body.appendChild(div);
+    let scrollWidth = div.offsetWidth - div.clientWidth;
+    div.remove();
+
+    return scrollWidth;
+  }
+
+  bindModal('.button-design', '.popup-design', '.popup-design .popup-close'); // вместо переменных передаем сразу селекторы. И функция становится универсальной для открытия окон при клике на другие тригеры.
+  bindModal('.button-consultation', '.popup-consultation', '.popup-consultation .popup-close'); // вместо переменных передаем сразу селекторы. И функция становится универсальной для открытия окон при клике на другие тригеры.
+
+  showModalByTime('.popup-consultation', 6000);
+
+};
+
+export default modals;

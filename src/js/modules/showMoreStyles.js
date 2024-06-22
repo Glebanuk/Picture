9ce9751b0429c6c77b1e00d454d0896962407761
gleb <glebanuk12387@gmail.com>
2024-06-22 15:44:58 +0300

    refactor: оптимизация в структуре foreach с использованием деструктуризации объекта

--- src/js/modules/showMoreStyles.js
+++ src/js/modules/showMoreStyles.js
@@ -27,23 +27,24 @@ const showMoreStylesjs = (triger, wrapper) =>  {
       });
 
     function createCards(response)  {
-      response.forEach(item =>  { // foreach используем потому что ответ (карточки) приходит в виде массива
+      response.forEach(({src, title, link}) =>  { // foreach используем потому что ответ (карточки) приходит в виде массива
         let card =document.createElement('div');
 
         card.classList.add('animated', 'fadeInUP', 'col-sm-3', 'col-sm-offset-0', 'col-xs-10', 'col-xs-offset-1');
 
         card.innerHTML  = `
         <div class="styles-block">
-           <img src=${item.src} alt = "style">
-           <h4>${item.title}</h4>
-           <a href=${item.link}}>Подробнее</a>
+           <img src=${src} alt = "style">
+           <h4>${title}</h4>
+           <a href=${link}}>Подробнее</a>
         </div>
-        `
+        `;
 
 
         document.querySelector(wrapper).appendChild(card);
       });
-    }
+    };
+
 
 
 
